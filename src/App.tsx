8e6b9cfa94b2c5f686a2a380ddb3c7448fe98f09
Patriyck <152425281+Patriyck@users.noreply.github.com>
2024-09-11 20:07:13 +0300

    add register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/home/home';
 import About from './pages/about/about';
+import Register from './pages/register/register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -11,16 +13,21 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/register">Register</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
+          <Route path= "/register" element={<Register/>} />
+          <Route path="/" element={<Register/>} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 
+  
 }
 
 export default App;
---
+++ src/types/images.d.ts
@@ -0,0 +1,16 @@
+// src/types/images.d.ts
+declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.jpeg" {
+    const value: string;
+    export default value;
+  }
+  
