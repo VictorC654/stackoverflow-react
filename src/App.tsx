d630090bc784a348538050b82d553d6d7a6ab2ae
Lena Chizil <159022742+lenachizil@users.noreply.github.com>
2024-09-12 16:07:07 +0300

    Add Topic List Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/home/home';
 import About from './pages/about/about';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import TopicList from './pages/topic-list/topic-list';
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/topic-list">Topic List</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
+          <Route path="/topic-list" element={<TopicList/>} />
         </Routes>
       </div>
     </BrowserRouter>
