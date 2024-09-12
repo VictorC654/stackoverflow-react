069fac00bafe45283df11029adddecadcbe2750c
ilianaPocropivnii <iuliana.pocropivnii@gmail.com>
2024-09-13 02:36:23 +0300

    add route and debug header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,27 @@
 import "./header.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (event: React.FormEvent) => {
+    event.preventDefault(); 
+    if (searchTerm.trim()) {
+      navigate(`/topic-list?query=${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+
   return (
     <div className="nav-container">
       <div className="navbar-custom">
         <div className="container">
-          <div className="site-name">Questify</div>
-          <form className="search-form">
+          <Link to="/home" className="site-name">
+            Questify
+          </Link>
+          <form className="search-form" onSubmit={handleSearchSubmit}>
             <div className="search-container">
               <div className="search-icon">
                 <i className="fas fa-search"></i>
@@ -16,12 +31,18 @@ export default function Header() {
                 placeholder="Ask something"
                 aria-label="Search"
                 className="search-input"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
             </div>
           </form>
           <div className="btn-custom">
-            <button className="btn-login">Log In</button>
-            <button className="btn-signup">Sign Up</button>
+            <Link to="/login" className="btn-login">
+              Log In
+            </Link>
+            <Link to="/register" className="btn-signup">
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
