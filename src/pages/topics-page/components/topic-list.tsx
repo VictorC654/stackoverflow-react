b2064ff771d2124c69ea14e0312720750f05f061
Lena Chizil <159022742+lenachizil@users.noreply.github.com>
2024-09-26 11:46:21 +0300

    Add tags

--- src/pages/topics-page/components/topic-list.tsx
+++ src/pages/topics-page/components/topic-list.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Col, Spinner } from 'react-bootstrap';
+import { Card, Row, Col, Spinner, Badge } from 'react-bootstrap';
 import './topic-list.css';
 import { Topic } from '../models/topicModel';
 import loupeImage from './img/loupe.png';
@@ -9,11 +9,11 @@ import { getTopics } from "services/apiService";
 const TOPIC_DISPLAY = 15;
 
 export default function TopicList() {
-  const [topics, setTopics] = useState<Topic[]>([]); 
-  const [visibleTopics, setVisibleTopics] = useState(TOPIC_DISPLAY); 
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [visibleTopics, setVisibleTopics] = useState(TOPIC_DISPLAY);
   const [loading, setLoading] = useState(true);
-  const [filteredTopics, setFilteredTopics] = useState<Topic[]>([]); 
-  const [totalFilteredTopics, setTotalFilteredTopics] = useState(0); 
+  const [filteredTopics, setFilteredTopics] = useState<Topic[]>([]);
+  const [totalFilteredTopics, setTotalFilteredTopics] = useState(0);
   const [loadingMore, setLoadingMore] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -24,25 +24,27 @@ export default function TopicList() {
 
   const loadMoreTopics = () => {
     setLoadingMore(true);
-    
+
     setTimeout(() => {
-      setVisibleTopics((prevVisibleTopics) => prevVisibleTopics + TOPIC_DISPLAY); 
+      setVisibleTopics((prevVisibleTopics) => prevVisibleTopics + TOPIC_DISPLAY);
       setLoadingMore(false);
-    }, 500); 
+    }, 500);
   };
-  
+
   useEffect(() => {
     const fetchTopics = async () => {
       try {
         const fetchedTopics = await getTopics();
         setTopics(fetchedTopics);
-        setFilteredTopics(fetchedTopics); 
-        setTotalFilteredTopics(fetchedTopics.length); 
+        setFilteredTopics(fetchedTopics);
+        setTotalFilteredTopics(fetchedTopics.length);
       } catch (err) {
         setError("Failed to fetch topics.");
         console.error(err);
       } finally {
-        setLoading(false);
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
       }
     };
     fetchTopics();
@@ -53,10 +55,10 @@ export default function TopicList() {
       const lowercasedTerm = searchTerm.toLowerCase();
       const filtered = topics.filter(topic => topic.title.toLowerCase().includes(lowercasedTerm));
       setFilteredTopics(filtered);
-      setTotalFilteredTopics(filtered.length); 
+      setTotalFilteredTopics(filtered.length);
     } else {
-      setFilteredTopics(topics); 
-      setTotalFilteredTopics(topics.length); 
+      setFilteredTopics(topics);
+      setTotalFilteredTopics(topics.length);
     }
   }, [searchTerm, topics]);
 
@@ -78,7 +80,11 @@ export default function TopicList() {
 
   return (
     <>
-      {filteredTopics.length === 0 ? (
+      {loading ? (
+        <div className="loading-container">
+          <Spinner animation="border" variant="dark" />
+        </div>
+      ) : filteredTopics.length === 0 ? (
         // No results found section
         <div className="topic-not-found-container">
           <Card className="card">
@@ -94,15 +100,13 @@ export default function TopicList() {
             </Card.Body>
           </Card>
         </div>
-      ) : null}
-
-      {/* Results section */}
-      {topics.length !== 0 && (
+      ) : (
+        // Results section
         <div className="topic-list-page">
           <div className="base-background">
             <div className="card-container">
               <div className="results-text">
-                Results ({totalFilteredTopics}) 
+                Results ({totalFilteredTopics})
               </div>
               <Row className={topics.length === 1 ? 'single-result-card' : ''}>
                 {visibleFilteredTopics.map((topic) => (
@@ -111,9 +115,16 @@ export default function TopicList() {
                       <Card.Body>
                         <Card.Title className="card-title">{topic.title}</Card.Title>
                         <Card.Text className="card-description">
-                        {truncateDescription(topic.description, 50)} 
+                          {truncateDescription(topic.description, 30)}
                         </Card.Text>
-                        <button className="answer-button">{topic.answers} Answers</button>
+                        <div className="tags-container">
+                          {topic.tags.map((tag, index) => (
+                            <Badge pill bg="secondary" key={index} className="tag-badge">
+                              {tag}
+                            </Badge>
+                          ))}
+                        </div>
+                        <button className="answer-button">Answers</button>
                       </Card.Body>
                     </Card>
                   </Col>
